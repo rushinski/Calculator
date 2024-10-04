ae0df5831880e2dd0b492a2ef498f7d0040b065d
Jacob Rushinski <dsrush2@verizon.net>
2024-10-04 09:21:46 -0400

    reformatting, backspace fix, and sqaure function

--- main.js
+++ main.js
@@ -1,99 +1,84 @@
 let arr = [];
 let str = "";
 // Inseration into bar
-let BAR = document.body.getElementsByClassName('bar')[0];
-let ele = document.createElement('p');
+let BAR = document.body.getElementsByClassName("bar")[0];
+let ele = document.createElement("p");
 BAR.appendChild(ele);
 
-function number(x)
-{
-    if (!isNaN( arr[arr.length - 1] ) && arr.length != 0) // If it's a number
-    {
-        arr[arr.length - 1] = Number( arr[arr.length - 1] + String(x) );
-    }
-    else
-        arr.push(Number(x));
-    str += x;
+function number(x) {
+  if (!isNaN(arr[arr.length - 1]) && arr.length != 0) {
+    // If it's a number
+    arr[arr.length - 1] = Number(arr[arr.length - 1] + String(x));
+  } else arr.push(Number(x));
+  str += x;
+  console.log(arr);
 
-    updateBar();
+  updateBar();
 }
 
 function updateBar() {
   ele.innerHTML = str;
 }
 
-function sign(sign) 
-{
-    arr.push(sign);
-    str += sign;
-    
-    updateBar();
+function sign(sign) {
+  arr.push(sign);
+  str += sign;
+
+  updateBar();
 }
 
-function calculate()
-{
-    let calc = 0;
-    let error = false;
+function calculate() {
+  let calc = 0;
+  let error = false;
 
-    if (!isNaN(arr[0]))
-    {
-        calc = arr[0];
-    }
-    else
-        error = true;
+  if (!isNaN(arr[0])) {
+    calc = arr[0];
+  } else error = true;
 
+  for (let i = 1; i < arr.length; i++) {
+    if (error === true) return "Error";
 
-    for (let i = 1; i < arr.length; i++)
-    {
-        if (error === true)
-            return "Error";
-
-        if (!isNaN(arr[i + 1]))        
-        {
-            if (arr[i] === "+") 
-            {
-                i++;
-                calc += arr[i];
-            }
-            else if (arr[i] === "-")
-            {
-                i++;
-                calc -= arr[i];
-            }
-            else if (arr[i] === "x")
-            {
-                i++;
-                calc *= arr[i]
-            }
-            else if (arr[i] === "÷")
-            {
-                i++;
-                calc /= arr[i];
-            }
-            else if (arr[i] === ".")
-            {
-                i++;
-                calc += Number("0." + arr[i]);
-            }
+    if (isNaN(arr[i])) {
+        if (!isNaN(arr[i] == "x²")) {
+            let loc = arr.indexOf("x²") - 1;
+            calc = arr[loc] * arr[loc];
         }
     }
 
-    arr = [calc];
-    str = String(calc);
-    updateBar();
-    console.log(calc);
+    if (!isNaN(arr[i + 1])) {
+      if (arr[i] === "+") {
+        i++;
+        calc += arr[i];
+      } else if (arr[i] === "-") {
+        i++;
+        calc -= arr[i];
+      } else if (arr[i] === "x") {
+        i++;
+        calc *= arr[i];
+      } else if (arr[i] === "÷") {
+        i++;
+        calc /= arr[i];
+      } else if (arr[i] === ".") {
+        i++;
+        calc += Number("0." + arr[i]);
+      } 
+    }
+  }
 
-    updateBar();
+  arr = [calc];
+  str = String(calc);
+  updateBar();
 }
 
 function clearBar() {
   arr = [];
-  str = '';
+  str = "";
   updateBar();
 }
 
 function clearLast() {
-  arr = arr.slice(0, -1);
   str = str.slice(0, -1);
+  arr = [];
+  arr.push(Number(str));
   updateBar();
 }
